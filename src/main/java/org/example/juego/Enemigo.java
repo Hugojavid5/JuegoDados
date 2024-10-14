eea95f50bb0be4757351a2989de0746058d420bf
Hugo <hugojavidferrero@gmail.com>
2024-10-14 10:22:37 +0200

    Clase Enemigo con javadoc

--- src/main/java/org/example/juego/Enemigo.java
+++ src/main/java/org/example/juego/Enemigo.java
@@ -1,6 +1,17 @@
 package org.example.juego;
+/**
+ * La clase { Enemigo} es una extensión de la clase {@code Personajes}
+ * que representa a un enemigo en el juego. Hereda todas las propiedades
+ * y métodos de la clase { Personajes}.
 
+ */
 public class Enemigo extends Personajes {
+
+    /**
+     * Crea un nuevo enemigo con el nombre y número de vidas especificados.
+     * @param nombre el nombre del enemigo
+     * @param vidas el número inicial de vidas del enemigo
+     */
     public Enemigo(String nombre, int vidas) {
         super(nombre, vidas);
     }
