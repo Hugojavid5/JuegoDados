2ae8df1830dfd76da64bdfa241e965cf692ce8c3
Hugo <hugojavidferrero@gmail.com>
2024-10-14 10:18:24 +0200

    Clase PersonajePrincipal con javadoc

--- src/main/java/org/example/juego/PersonajePrincipal.java
+++ src/main/java/org/example/juego/PersonajePrincipal.java
@@ -1,6 +1,16 @@
 package org.example.juego;
-
+/**
+ * La clase { PersonajePrincipal} es una extensión de la clase {@code Personajes}
+ * que representa al personaje principal en el juego. Hereda todas las propiedades
+ * y métodos de { Personajes}.
+ */
 public class PersonajePrincipal extends Personajes {
+
+    /**
+     * Crea un nuevo personaje principal con el nombre y número de vidas especificados.
+     * @param nombre el nombre del personaje principal
+     * @param vidas el número inicial de vidas del personaje principal
+     */
     public PersonajePrincipal(String nombre, int vidas) {
         super(nombre, vidas);
     }
